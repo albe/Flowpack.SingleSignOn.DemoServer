89f241c53d4125e6f0d6d4886f88e99754f0927d
Christopher Hlubek <hlubek@networkteam.com>
2012-10-22 13:58:02 +0200

    [TASK] Initial commit

---
+++ Classes/Acme/DemoServer/Controller/StandardController.php
@@ -0,0 +1,31 @@
+<?php
+namespace Acme\DemoServer\Controller;
+
+/*                                                                        *
+ * This script belongs to the TYPO3 Flow package "Acme.DemoServer".       *
+ *                                                                        *
+ *                                                                        */
+
+use TYPO3\Flow\Annotations as Flow;
+
+/**
+ * Standard controller for the Acme.DemoServer package 
+ *
+ * @Flow\Scope("singleton")
+ */
+class StandardController extends \TYPO3\Flow\Mvc\Controller\ActionController {
+
+	/**
+	 * Index action
+	 *
+	 * @return void
+	 */
+	public function indexAction() {
+		$this->view->assign('foos', array(
+			'bar', 'baz'
+		));
+	}
+
+}
+
+?>
