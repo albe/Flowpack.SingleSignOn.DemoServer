ce9f3903d45b6c7b5462987b7cac11b63f2e9e1c
Christopher Hlubek <hlubek@networkteam.com>
2012-10-24 10:51:34 +0200

    [TASK] Implement bootstrap based layout for SSO demo server

---
+++ Classes/Acme/DemoServer/Controller/LoginController.php
@@ -0,0 +1,56 @@
+<?php
+namespace Acme\DemoServer\Controller;
+
+/*                                                                        *
+ * This script belongs to the TYPO3 Flow package "Acme.DemoServer".       *
+ *                                                                        *
+ *                                                                        */
+
+use TYPO3\Flow\Annotations as Flow;
+
+/**
+ * Login controller for local authentication
+ *
+ * This controller will be called if no account was authenticated
+ * on the SSO server.
+ *
+ * @Flow\Scope("singleton")
+ */
+class LoginController extends \TYPO3\Flow\Security\Authentication\Controller\AbstractAuthenticationController {
+
+	/**
+	 * Render a login form
+	 */
+	public function indexAction() {
+
+	}
+
+	/**
+	 * Is called if authentication was successful
+	 *
+	 * @param \TYPO3\Flow\Mvc\ActionRequest $originalRequest The request that was intercepted by the security framework, NULL if there was none
+	 * @return string
+	 */
+	protected function onAuthenticationSuccess(\TYPO3\Flow\Mvc\ActionRequest $originalRequest = NULL) {
+		if ($originalRequest !== NULL) {
+			$this->redirectToRequest($originalRequest);
+		}
+
+		$this->addFlashMessage('Could not redirect to original request', 'Error in authentication', \TYPO3\Flow\Error\Message::SEVERITY_WARNING);
+		$this->redirect('index', 'Standard');
+	}
+
+	/**
+	 * Logout
+	 *
+	 * @return void
+	 */
+	public function logoutAction() {
+		parent::logoutAction();
+
+		$this->addFlashMessage('You have been logged out');
+		$this->redirect('index', 'Standard');
+	}
+
+}
+?>
--- Classes/Acme/DemoServer/Controller/StandardController.php
+++ Classes/Acme/DemoServer/Controller/StandardController.php
@@ -21,6 +21,7 @@ class StandardController extends \TYPO3\Flow\Mvc\Controller\ActionController {
 	 * @return void
 	 */
 	public function indexAction() {
+
 	}
 
 }
