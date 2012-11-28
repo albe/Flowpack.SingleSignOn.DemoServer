bafa7091edbc9164c12f6501b4dd772cbcc96a5a
Christopher Hlubek <hlubek@networkteam.com>
2012-11-28 21:56:07 +0100

    [FEATURE] Implement global session overview

---
+++ Classes/Acme/DemoServer/Controller/SessionsController.php
@@ -0,0 +1,48 @@
+<?php
+namespace Acme\DemoServer\Controller;
+
+/*                                                                        *
+ * This script belongs to the TYPO3 Flow package "Acme.DemoServer".       *
+ *                                                                        *
+ *                                                                        */
+
+use TYPO3\Flow\Annotations as Flow;
+
+/**
+ * SSO server sessions controller
+ *
+ * @Flow\Scope("singleton")
+ */
+class SessionsController extends \TYPO3\Flow\Mvc\Controller\ActionController {
+
+	/**
+	 * @Flow\Inject
+	 * @var \TYPO3\Flow\Session\SessionManagerInterface
+	 */
+	protected $sessionManager;
+
+	/**
+	 * Display sessions
+	 */
+	public function indexAction() {
+		$activeSessions = $this->sessionManager->getActiveSessions();
+		$this->view->assign('activeSessions', $activeSessions);
+	}
+
+	/**
+	 * Destroy a session
+	 *
+	 * @param \TYPO3\Flow\Session\Session $session
+	 * @Flow\IgnoreValidation("$session")
+	 */
+	public function destroyAction(\TYPO3\Flow\Session\Session $session) {
+		$session->destroy('Through web interface');
+
+		$this->addFlashMessage('Session destroyed');
+
+		$this->redirect('index');
+	}
+
+}
+
+?>
